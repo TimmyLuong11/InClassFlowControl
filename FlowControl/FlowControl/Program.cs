02c218e28d8d30c3c1360c720bb50f764b9153c9
Timothy Luong <Timothy.Luong@ou.edu>
2021-02-09 17:38:16 -0600

    Added in class example

--- FlowControl/FlowControl/Program.cs
+++ FlowControl/FlowControl/Program.cs
@@ -7,6 +7,48 @@ namespace FlowControl
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int var1 = 0;
+            if (var1 ==1)
+            {
+                //Do something
+            }
+            else if (var1 == 2)
+            {
+                //do something
+            }
+            else if (var1 == 3 || var1 == 4)
+            {
+                //Do something
+            }
+            else
+            {
+                //Do something 
+            }
+
+            const string myName = "Benjamin";
+            const string niceName = "Andrea";
+            const string sillyName = "Ploppy";
+            string name;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            switch(name.ToLower())
+            {
+                case myName:
+                    Console.WriteLine("You have the same name as me!");
+                    break;
+                case niceName:
+                    Console.WriteLine("My, what a nice name you have!");
+                    break;
+                case sillyName:
+                    Console.WriteLine("That's a very silly name.");
+                    break;
+            }
+            Console.WriteLine("That's a very silly name.");
+            Console.ReadKey();
+
+            int myInterger = 0;
+            string resultString = (myInterger < 10) ? "Less than 10" : "Greather than or equal to 10";
         }
     }
 }
