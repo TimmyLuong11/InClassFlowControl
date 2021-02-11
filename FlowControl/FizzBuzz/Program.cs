338c2b38a21ec4325ba412d2d7a9196cb4a85dc1
Timothy Luong <Timothy.Luong@ou.edu>
2021-02-11 17:39:32 -0600

    Added FizzBuzz

---
+++ FlowControl/FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generate a random number between 1 and 100
+            Random rand = new Random();
+            int randNum = rand.Next(1, 101);
+            int divisibleBy3Remainder = randNum % 3;
+            bool isDivisibleBy3 = divisibleBy3Remainder % 3 == 0;
+
+            //randNum = 20;
+            if (randNum % 3 == 0 && randNum % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else if (randNum % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (randNum % 3 == 0) // == true)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(randNum);
+            }
+        }
+    }
+}
