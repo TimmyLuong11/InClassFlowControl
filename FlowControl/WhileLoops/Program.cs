c96cd3dd6d8c8cc454569ba67d3140e64794ea77
Timothy Luong <Timothy.Luong@ou.edu>
2021-02-23 18:05:35 -0600

    Added WhileLoops

---
+++ FlowControl/WhileLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your lucky #>>");
+            string answer = Console.ReadLine();
+            int luckyNum;
+            while (int.TryParse(answer, out luckyNum) == false)
+            {
+                Console.WriteLine("Sory ou enter an invalid #. Please try again");
+                answer = Console.ReadLine();
+                
+            }
+            Console.WriteLine("Yay you know how to enter in a number");
+        }
+    }
+}
